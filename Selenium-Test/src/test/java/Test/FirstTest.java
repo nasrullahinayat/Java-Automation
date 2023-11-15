d7c44ba3b64f91625f22033d1cab8461596b6dae
Nasrullah <nasrullah.inayat@live.com>
2023-11-15 17:51:54 +0500

    First Test

---
+++ Selenium-Test/src/test/java/Test/FirstTest.java
@@ -0,0 +1,19 @@
+package Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//System.out.println("Hello");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.navigate().to("https://google.com");
+		driver.close();
+
+	}
+
+}
